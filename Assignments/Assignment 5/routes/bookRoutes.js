75dcec04191b8708809332a5d48a6cd83400ac4d
sandhya34109 <sandhyadeva0907@gmail.com>
2025-12-10 10:28:50 +0530

    Add files via upload

---
+++ Assignments/Assignment 5/routes/bookRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createBook,
+  getBooks,
+  getBookById,
+  updateBook,
+  deleteBook
+} = require("../controllers/bookControllers");
+
+router.post("/", createBook);
+router.get("/", getBooks);
+router.get("/:id", getBookById);
+router.put("/:id", updateBook);
+router.delete("/:id", deleteBook);
+
+module.exports = router;
